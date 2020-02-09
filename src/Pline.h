8920d348630633ecd3925185a00e0161983407ea
Sabaniki <sawada13@mccc.jp>
2020-02-09 17:55:45 +0900

    端が黒の際に左右でモータスピードが違うバグを修正

--- src/Pline.h
+++ src/Pline.h
@@ -7,8 +7,8 @@ const int f = 2;
 const int s = 3;
 
 //const int powerArray[9] = {400, 300, 250, 150, 0, 150, 250, 300, 450};
-const int powerArray[9] = {400, 300, 250, 150, 0, 150, 250, 300, 450};
-const int standardValue = 250;
+const int powerArray[9] = {400, 300, 250, 150, 0, 150, 250, 300, 400};
+const int standardValue = 255;
 
 //MOVE関数の中で直進とかいちいちMOVE(255,255)って書くのめんどいので。
 void GO(int way);
